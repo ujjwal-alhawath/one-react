be4acdc7e5edc459f5e109e95da54437e44cdadf
ujjwal-alhawath <ujjwalahlawath@gmail.com>
2026-02-24 16:03:43 +0530

    some problem in capatilization

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'  
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Header from './components/header'
+import Header from './components/Header'
 import './styles/app.scss'
 import'./styles/header.scss'
 import'./styles/home.scss'
 import Home from './components/home'
-import contact from './components/contact'
+import Contact from './components/contact'
 import'./styles/contact.scss'
 import Services from './components/service'
 import'./styles/service.scss'
@@ -19,7 +19,7 @@ function App() {
     <Header/>
     <Routes>
        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<contact />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
     </Routes>
    </Router>
